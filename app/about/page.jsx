71e4fedc307f4b1bf6aac13aa0bbe193095c7cf3
Cecilia <ccchimento@gmail.com>
2024-04-03 10:27:13 -0600

    about styling

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,23 +1,61 @@
 import Image from "next/image";
 
 export default function Page() {
-  const skills = ["JavaScript", "React", "Next.js", "Python", "Flask", "SQL", "SQLite", "SQLAlchemy", "HTML", "CSS", "Git", "GitHub", "RESTful APIs", "Miro", "Figma", "Chart.js", "Beautiful Soup", "Material UI", "Semantic UI", "Tailwind CSS"]
-  
-  for (let i = 0; i < skills.length - 1; i++){
-    skills[i] += (" • ")
-    console.log(skills[i])
+  const skills = [
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Python",
+    "Flask",
+    "SQL",
+    "SQLite",
+    "SQLAlchemy",
+    "HTML",
+    "CSS",
+    "Git",
+    "GitHub",
+    "RESTful APIs",
+    "Miro",
+    "Figma",
+    "Chart.js",
+    "Beautiful Soup",
+    "Material UI",
+    "Semantic UI",
+    "Tailwind CSS",
+  ];
+
+  for (let i = 0; i < skills.length - 1; i++) {
+    skills[i] += " • ";
+    console.log(skills[i]);
   }
 
-  const skillsDisplay = skills.map(skill => <li key={skill} className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">{skill}</li>)
-  
+  const skillsDisplay = skills.map((skill) => (
+    <li
+      key={skill}
+      className="text-sm tracking-tight text-captions sm:text-base text-left mt-3"
+    >
+      {skill}
+    </li>
+  ));
+
   return (
     <>
       <h1 className="text-3xl font-bold tracking-tight text-headers">
         About Me
       </h1>
-      <div className="flex gap-8">
-        <div>
-          <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-6 text-right">
+      <div className="grid grid-cols-2 md:grid-cols-3 mt-4">
+      <div className="col-span-2 flex justify-center md:col-span-1 md:justify-start">
+          <Image
+            className="rounded shadow shadow-color-[#4c5657]"
+            src="/about_me.png"
+            width={450}
+            height={562}
+            alt="Cecilia Chimento head shot"
+            priority="false"
+          />
+        </div>
+        <div className="col-span-2">
+          <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-2 text-right">
             my story{" "}
           </h2>
           <p className="text-base font-bold tracking-tight text-captions sm:text-lg text-left">
@@ -41,8 +79,8 @@ export default function Page() {
           </p>
           <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
             From completing an accelerated Bachelor of Science degree Summa Cum
-            Laude to caring for our country&apos;s most critical patients, I have
-            never met a challenge that I haven&apos;t fully embraced and
+            Laude to caring for our country&apos;s most critical patients, I
+            have never met a challenge that I haven&apos;t fully embraced and
             overcome, and embarking on this new mission is no different.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-6 text-right">
@@ -52,15 +90,6 @@ export default function Page() {
             {skillsDisplay}
           </ul>
         </div>
-
-        <Image
-          className="rounded shadow shadow-color-[#4c5657] float-right"
-          src="/about_me.png"
-          width={450}
-          height={562}
-          alt="Cecilia Chimento head shot"
-          priority="false"
-        />
       </div>
     </>
   );

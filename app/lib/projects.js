e605dae45015eee487b60c34e66e71cd8a7deb31
Cecilia <ccchimento@gmail.com>
2024-04-22 17:15:42 -0600

    projects added

---
+++ app/lib/projects.js
@@ -0,0 +1,42 @@
+const projects = [
+  {
+    title: "EMpowR",
+    description:
+      "A platform for users to store healthcare data in one convenient location, empowering users to take control of their health.",
+    tech: "JavaScript, React, Python, Flask, SQLAlchemy",
+    image: "/EMpowR_dashboard.png",
+    demo: "https://www.loom.com/share/ba7959c85ce84475acad6320efda1948",
+  },
+  {
+    title: "FloraFriends",
+    description:
+      "A social media inspired platform for plant owners to track the care they have provided for their plants.",
+    tech: "JavaScript, React, Python, Flask, SQLAlchemy",
+    image: "/FloraFriends.png",
+    demo: "https://www.loom.com/share/26b6865b18d1469f863b2686266e60c9",
+  },
+  {
+    title: "QuestForge",
+    description:
+      "An interactive text-based fantasy adventure game where users manage a party of adventurers.",
+    tech: "Python, SQL, SQLAlchemy, CLI, rich",
+    image: "/QuestForge.png",
+    demo: "https://www.loom.com/share/8c3d53764b564dd1858ee1153aaeedcb",
+  },
+  {
+    title: "National Parks of California",
+    description:
+      "A comprehensive platform dedicated to US National Parks located in California, allowing users to plan their next trip",
+    tech: "JavaScript, React, JSON server",
+    image: "/NPCali.png",
+  },
+  {
+    title: "Harry Potter Book Reviewer",
+    description:
+      "A site allowing users to rate, review and learn about each of the Harry Potter novels.",
+    tech: "JavaScript, JSON server",
+    image: "/HarryPotter.png",
+  },
+];
+
+module.exports = {projects}
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,36 +1,49 @@
-export default function Page() {
-  const projects = [
-    {
-      title: "EMpowR",
-      description:
-        "A platform for users to store healthcare data in one convenient location, empowering users to take control of their health.",
-      tech: "JavaScript, React, Python, Flask, SQLAlchemy",
-    },
-    {
-      title: "FloraFriends",
-      description:
-        "A social media inspired platform for plant owners to track the care they have provided for their plants.",
-      tech: "JavaScript, React, Python, Flask, SQLAlchemy",
-    },
-    { title: "QuestForge", description: "" },
-    { title: "National Parks of California", description: "" },
-    { title: "Harry Potter Book Reviewer", description: "" },
-  ];
+import Image from "next/image";
+import projects from "../lib/projects"
 
+export default function Page() {
+  // console.log(projects)
   return (
     <>
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         Projects
       </h1>
-      {projects.map((project) => (
-        <div key={project.title}>
-          <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">
-            {project.title}
-          </h3>
-          <p className="text-gray-900">{project.description}</p>
-          <p className="text-gray-900">{project.tech}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-2 gap-4">
+        {projects.projects.slice(0,2).map((project) => (
+          <div key={project.title}>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+              {project.title}
+            </h3>
+            <p className="text-gray-900">{project.description}</p>
+            <p className="text-gray-900">{project.tech}</p>
+            <Image
+              src={project.image}
+              height={282}
+              width={500}
+              alt={`Screenshot of ${project.title}`}
+              priority='false'
+            />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        {projects.projects.slice(2).map((project) => (
+          <div key={project.title}>
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+              {project.title}
+            </h3>
+            <p className="text-gray-900">{project.description}</p>
+            <p className="text-gray-900">{project.tech}</p>
+            <Image
+              src={project.image}
+              height={227}
+              width={400}
+              alt={`Screenshot of ${project.title}`}
+              priority='false'
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }

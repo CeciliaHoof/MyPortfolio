8d82757383f1e48a379ab95bd32101edca40c142
Cecilia <ccchimento@gmail.com>
2024-04-26 15:34:25 -0600

    about me

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,17 +1,42 @@
 export default function Page() {
   return (
     <>
-      <h1 className="text-2xl font-bold tracking-tight text-headers sm:text-4xl">
+      <h1 className="text-3xl font-bold tracking-tight text-headers">
         About Me
       </h1>
-      <p className="text-2xl tracking-tight text-captions sm:text-xl w-7/12 m-auto mt-40 text-center">
-        <span className="text-2xl font-bold tracking-tight text-sub-headers sm:text-3xl block">
-          Loading...{" "}
-        </span>
-        I thought that this section would be the easy part, but it turns out
-        that&apos;s not the case.  Check back soon!{" "}
+      <div className="w-1/2">
+        <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-6">
+          My Story{" "}
+        </h2>
+        <p className="text-base font-bold tracking-tight text-captions sm:text-lg text-left">
+          Hi, I&apos;m Cecilia, and I&apos;m a Software Engineer on a mission.
+        </p>
+        <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
+          For the past five years, I&apos;ve been a Registered Nurse in adult
+          and neonatal intensive care units, where collaboration and
+          problem-solving were daily essentials. During this time, I also
+          worked as a COVID-19 crisis response nurse, which reinforced my skills
+          for working in fast-paced, high-intensity environments.
+        </p>
+        <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
+          Witnessing firsthand the potential for technology and innovation to
+          revolutionize healthcare, I embarked on a journey to marry my medical
+          expertise with cutting-edge software development skills. With the
+          arsenal gained from the Flatiron School Full Stack Software
+          Engineering program and my rich medical background, I&apos;m poised to
+          be a driving force in reshaping how technology enhances our healthcare
+          experiences.
+        </p>
+        <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
+          From completing an accelerated Bachelor of Science degree Summa Cum Laude
+          to caring for our country&apos;s sickest patients, I have never
+          met a challenge that I haven&apos;t fully embraced and overcome, and
+          embarking on this new mission is no different.
+        </p>
+      </div>
+      <p className="text-2xl tracking-tight text-captions sm:text-xl w-5/12 m-auto mt-40 text-left">
         <span className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl block mt-10">
-          Meanwhile, explore my projects and blog posts and let&apos;s connect!{" "}
+          More to come! Meanwhile, explore my projects and blog posts and let&apos;s connect!{" "}
         </span>
       </p>
     </>

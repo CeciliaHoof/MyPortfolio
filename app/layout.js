0fe5084c1bc07dedfdb29238537b587caed59c3d
Cecilia <ccchimento@gmail.com>
2024-05-06 16:05:26 -0600

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,28 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./ui/nav";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Cecilia Chimento",
   description: "Cecilia Chimento's software development portfolio.",
-  fullscreen: "allow"
+  fullscreen: "allow",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="bg-[#d5d5d5] min-h-screen">
-          <Nav />
-          <div className="relative isolate px-6 pt-16 lg:px-8">{children}</div>
-        </div>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="bg-[#d5d5d5] min-h-screen">
+            <Nav />
+            <div className="relative isolate px-6 pt-16 lg:px-8">
+              {children}
+              <Analytics />
+            </div>
+          </div>
+        </body>
+      </html>
   );
 }

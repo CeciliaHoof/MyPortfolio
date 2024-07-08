2b10d34928a81d6e5c80637e93883268bae6ebc7
Cecilia <ccchimento@gmail.com>
2024-07-08 16:32:41 -0600

    about image replaced

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import photo from "../../public/about_me.png"
+import photo from "../../public/headshot.png";
 import { skills } from "../lib/skills";
 
 export default function Page() {
@@ -19,28 +19,38 @@ export default function Page() {
         About Me
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-3 mt-4">
-        <div className="col-span-2 md:col-span-1 block">
+        <div className="col-span-2 md:col-span-1 block sm:ml-0 sm:mr-5">
           <Image
-            className="rounded shadow shadow-color-[#4c5657] m-auto md:ml-0"
+            className="rounded shadow shadow-color-[#4c5657] m-auto "
             src={photo}
             alt="Cecilia Chimento head shot"
-            priority="true"
-            // layout="responsive"
+            priority
+            height={562}
+            width={450}
           />
+          <Link
+            href="/Cecilia_Chimento_Resume.pdf"
+            download={true}
+            target={"_blank"}
+            className="text-xl font-bold tracking-tight text-captions block mt-6 text-center mb-4"
+          >
+            View Resume
+          </Link>
         </div>
-        <div className="col-span-2 pl-2">
-          <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-2 text-right">
+        <div className="col-span-2">
+          <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-2 sm:mt-0 text-right">
             my story{" "}
           </h2>
           <p className="text-base font-bold tracking-tight text-captions sm:text-lg text-left">
             Hi, I&apos;m Cecilia, and I&apos;m a Software Engineer on a mission.
           </p>
           <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
-            For the past five years, I&apos;ve been a Registered Nurse in adult and
-            neonatal intensive care units, where collaboration, problem-solving,
-            and attention to detail were daily essentials. During this time, I
-            also worked as a COVID-19 crisis response nurse, honing my ability
-            to thrive in fast-paced, high-intensity environments.
+            For the past five years, I&apos;ve been a Registered Nurse in adult
+            and neonatal intensive care units, where collaboration,
+            problem-solving, and attention to detail were daily essentials.
+            During this time, I also worked as a COVID-19 crisis response nurse,
+            honing my ability to thrive in fast-paced, high-intensity
+            environments.
           </p>
           <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
             Inspired by the transformative potential of technology, I embarked
@@ -58,25 +68,17 @@ export default function Page() {
             challenges.
           </p>
           <p className="text-sm tracking-tight text-captions sm:text-base text-left mt-3">
-            I&apos;m passionate about leveraging technology to create solutions that
-            drive meaningful impact. Whether it&apos;s improving user experiences,
-            optimizing processes, or developing new features, I&apos;m dedicated to
-            delivering high-quality, scalable software.
+            I&apos;m passionate about leveraging technology to create solutions
+            that drive meaningful impact. Whether it&apos;s improving user
+            experiences, optimizing processes, or developing new features,
+            I&apos;m dedicated to delivering high-quality, scalable software.
           </p>
           <h2 className="text-2xl font-bold tracking-tight text-sub-headers block mt-6 text-right">
             my skills{" "}
           </h2>
-          <ul className="flex flex-wrap items-center justify-center gap-1">
+          <ul className="flex flex-wrap items-center justify-center gap-1 mb-3">
             {skillsDisplay}
           </ul>
-          <Link
-            href="/Cecilia_Chimento_Resume.pdf"
-            download={true}
-            target={"_blank"}
-            className="text-2xl font-bold tracking-tight text-captions block mt-6 text-center mb-4"
-          >
-            View Resume
-          </Link>
         </div>
       </div>
     </>
